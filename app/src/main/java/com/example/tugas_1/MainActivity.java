27d9b0f4e9f8dbc5e73eb8ff6d282dcb67fe0167
Made Umbu Paskalis Putra Arimbawa <made.umbu@ti.ukdw.ac.id>
2020-02-13 23:43:56 +0700

    first commit

---
+++ app/src/main/java/com/example/tugas_1/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.tugas_1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        Button tombol = findViewById(R.id.button);
+//
+//        tombol.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                TextView textView = findViewById(R.id.text);
+//                if(textView.getText().equals("Hello World!")){
+//                    textView.setText("ASU");
+//                }else{
+//                    textView.setText("Hello World!");
+//                }
+//
+//            }
+//        });
+    }
+}
