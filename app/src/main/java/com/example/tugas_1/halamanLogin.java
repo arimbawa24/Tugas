91daa6be7f80acd7b9bdc271fe3de8b4abf0a46c
Made Umbu Paskalis Putra Arimbawa <made.umbu@ti.ukdw.ac.id>
2020-02-28 18:35:30 +0700

    latihan
    latihan

--- app/src/main/java/com/example/tugas_1/halamanLogin.java
+++ app/src/main/java/com/example/tugas_1/halamanLogin.java
@@ -1,6 +1,7 @@
 package com.example.tugas_1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +11,10 @@ public class halamanLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_halaman_login);
+
+        FragmentTransaction fragmentTransaction = this.getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_holder, new Fragment1());
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
     }
 }
