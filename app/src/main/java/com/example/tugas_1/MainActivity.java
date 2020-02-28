d5c5c3817364223e148cf69e8cf318416127e340
Made Umbu Paskalis Putra Arimbawa <made.umbu@ti.ukdw.ac.id>
2020-02-28 17:23:21 +0700

    tugas 2 (project kehapus)
    tugas 2(kehapus)

--- app/src/main/java/com/example/tugas_1/MainActivity.java
+++ app/src/main/java/com/example/tugas_1/MainActivity.java
@@ -2,30 +2,33 @@ package com.example.tugas_1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button button;
+    EditText editText1;
+    EditText editText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-//        Button tombol = findViewById(R.id.button);
-//
-//        tombol.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                TextView textView = findViewById(R.id.text);
-//                if(textView.getText().equals("Hello World!")){
-//                    textView.setText("ASU");
-//                }else{
-//                    textView.setText("Hello World!");
-//                }
-//
-//            }
-//        });
+        button = findViewById(R.id.btn_Login);
+        editText1 = findViewById(R.id.txt_edit);
+        editText2 = findViewById(R.id.txt_edit2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = editText1.getText().toString();
+                String pass = editText2.getText().toString();
+                Toast.makeText(getApplicationContext(),"Email: " +email+ " Password :" + pass,Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(MainActivity.this, halamanLogin.class);
+                startActivity(intent);
+            }
+        });
     }
 }
