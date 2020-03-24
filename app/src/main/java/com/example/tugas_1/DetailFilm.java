c9c14d9593d126a173ac5bc416226f776c60b9a0
Made Umbu Paskalis Putra Arimbawa <made.umbu@ti.ukdw.ac.id>
2020-03-24 18:25:57 +0700

    recycle

---
+++ app/src/main/java/com/example/tugas_1/DetailFilm.java
@@ -0,0 +1,43 @@
+package com.example.tugas_1;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class DetailFilm extends AppCompatActivity {
+    String data1, data2;
+    int images;
+    ImageView mainImageView;
+    TextView judul,deskripsi;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail);
+
+        mainImageView = findViewById(R.id.gambarDetail);
+        judul = findViewById(R.id.judulFilmDetail);
+        deskripsi = findViewById(R.id.deskripsiFilmDetail);
+
+        getData();
+        setData();
+    }
+
+    private void getData(){
+        if(getIntent().hasExtra("images") && getIntent().hasExtra("data1") && getIntent().hasExtra("data2")){
+            data1 = getIntent().getStringExtra("data1");
+            data2 = getIntent().getStringExtra("data2");
+            images = getIntent().getIntExtra("images",1);
+        }else{
+            Toast.makeText(getApplicationContext(),"No Data",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void setData(){
+        judul.setText(data1);
+        deskripsi.setText(data2);
+        mainImageView.setImageResource(images);
+    }
+}
--- app/src/main/java/com/example/tugas_1/Status.java
+++ app/src/main/java/com/example/tugas_1/Status.java
@@ -6,11 +6,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class Status extends Fragment {
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedIntancesState){
+    RecyclerView recyclerView;
+    String s1[], s2[];
+    int images[] = {R.drawable.blood_shot,R.drawable.janin,R.drawable.gundala,R.drawable.wulan};
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstancesState){
         View view = inflater.inflate(R.layout.fragment_status,container,false);
+        s1 = getResources().getStringArray(R.array.nama_film);
+        s2 = getResources().getStringArray(R.array.Deskripsi);
+        recyclerView = (RecyclerView) view.findViewById(R.id.recycler);
+        RecycleAdapter recyclerviewAdapter = new RecycleAdapter(getContext(),s1,s2,images);
+        recyclerView.setAdapter(recyclerviewAdapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
         return view;
     }
 }
